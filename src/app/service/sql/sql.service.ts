0761735a2233ed5a2b2241d821d6238bfb4815fe
Arnob <arnobxtreme@gmail.com>
2018-09-30 16:08:35 +0600

    stock updating not complete 4.08 30.9.18

--- src/app/service/sql/sql.service.ts
+++ src/app/service/sql/sql.service.ts
@@ -7,9 +7,10 @@ export class SqlService {
   url_L = 'http://localhost';     // Local Requset
   url_P = 'http://web.revolution-bd.com';     // Online // Don't Change the Domain
 
+  postReturnJsonLink = 'phpQuery/queryArgRJson.php';
   // Set public and local URL
   // Change before Build
-  url = this.url_L;
+  url = this.url_P;
 
   // For All url
   url_t = this.url + '/RBDesktopSoft/storePHP/';
@@ -28,8 +29,27 @@ export class SqlService {
     return this.http.get(this.url_t + Linkext);
   }
 
+  /**
+   * @function ping()
+   *
+   * This function is looking for is there any internet
+   * Available or not
+   */
   ping() {
    return this.http.get(this.url);
+  }
+
+
+
+  /**
+   * postQry(sql, where)
+   * @param sql 'Sql Query pass'
+   * @param where 'where to query'
+   */
+  postQry(sql, where) {
+    return this.postRequest(where, sql)
+    .map((response) => response.json())
+    .toPromise();
   }
 
    // Encode To base64
--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -1,6 +1,9 @@
 import { MessageService } from 'primeng/components/common/messageservice';
 import { Component, OnInit } from '@angular/core';
 import { SqlService } from '../service/sql/sql.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import { UUID } from 'angular2-uuid';
 
 @Component({
   selector: 'app-stock',
@@ -10,13 +13,27 @@ import { SqlService } from '../service/sql/sql.service';
 export class StockComponent implements OnInit {
   display: Boolean = false;
 
-  // result work
-  inResult: { [k: string]: any } = {};
-  inResult2: { [k: string]: any } = {};
-  typeArray = [];
-  typeArrayTemp: { [k: string]: any } = {};
-  typeSFTtotalArray = [];
+  // input info
+  details: any;
+  media: any;
+  size: any;
+  mediaWhere: any;
+
+  // remaining Calculate
+  tempVal1: { [k: string]: any } = {};
+  tempVal2: { [k: string]: any } = {};
+  totalIn: { [k: string]: any } = {};
+  totalout: { [k: string]: any } = {};
+  remain: { [k: string]: any } = {};
+  INsum = 0;
+  OUTsum = 0;
+
+  // media work
+  typeAndSumResult = [];
   inShow = true;
+
+  // stock
+  stock = [];
 
   constructor(
     private message: MessageService,
@@ -25,6 +42,7 @@ export class StockComponent implements OnInit {
 
   ngOnInit() {
     this.getTypeFromDB();
+    this.getStockDB();
 
   }
 
@@ -32,73 +50,100 @@ export class StockComponent implements OnInit {
     this.display = true;
   }
 
-  SQLRequest(v1, v2, v3, v4) {
+  isNumber() {
+    if (!Number(this.size)) {
+      this.size = undefined;
+    }
+  }
 
-    /*
-    v1 = porpose
-    v2 = details
-    v3 = amount
-    v4 = type
-    */
-    //   this.message.add({ severity: 'info', summary: 'Processing', detail: 'Sending to DB' });
-    //   // tslint:disable-next-line:max-line-length
+  remainingCalculate() {
+    this.tempVal1 = this.typeAndSumResult;
+    this.tempVal2 = this.stock;
+    let inSum = 0;
+    let outSum = 0;
+    for (let i = 0; i < this.tempVal2.length; i++) {
+      if (this.tempVal2[i].mediawhere === 'IN') {
+        inSum = inSum + Number(this.tempVal2[i].size);
+      }
+      if (this.tempVal2[i].mediawhere === 'OUT') {
+        outSum = outSum + Number(this.tempVal2[i].size);
+      }
+    }
+    this.INsum = inSum;
+    this.OUTsum = outSum;
+
+    // console.log({inSum, outSum});
+  }
+
+  postToStockDB() {
     // tslint:disable-next-line:max-line-length
-    //   const sql = { 'sql': 'INSERT INTO CashINOUT (entryid, porpose, details, amount, type, updateby, CreatedTime, CreatedDate) VALUES ("' + this.getRandomID() + '", "' + v1 + '", "' + v2 + '", "' + v3 + '", "' + v4 + '","' + this.cookie.get('username') + '", "' + this.geTime() + '", "' + this.geDate() + '")' };
-    //   // console.log(sql);
-    //   this.sql.postRequest('anySql/anySql.php', sql).subscribe(
-    //     response => {
-    //       // console.log(response.json());
-    //       if (response.json()[0].status === 'Done') {
-    //         this.message.add({ severity: 'success', summary: 'Recorded', detail: 'Store To DB' });
-    //       }
-    //     },
-    //     err => {
-    //       console.log(err);
-    //       this.message.add({ severity: 'error', summary: 'Problem Found', detail: err });
-    //     }
-    //   );
-    // }
+    const sql = { 'sql': 'INSERT INTO stock(id, details, media, size, mediawhere, created_time, created_date) VALUES ("' + this.getRandomID() + '", "' + this.details + '", "' + this.media + '", "' + this.size + '", "' + this.mediaWhere + '", "' + this.geTime() + '", "' + this.geDate() + '")' };
+    this.sql.postQry(sql, 'phpQuery/queryArgRDone.php').then((result) => {
+      console.log(result);
+      this.getStockDB();
+      this.remainingCalculate();
+      this.clearinput();
+    });
+  }
+
+  DeleteFromStockDB(value) {
+    // tslint:disable-next-line:max-line-length
+    const sql = { 'sql': 'DELETE FROM stock WHERE id = "' + value + '"' };
+    this.sql.postQry(sql, 'phpQuery/queryArgRDone.php').then((result) => {
+      console.log(result);
+      this.getStockDB();
+    });
+  }
+
+  getStockDB() {
+    // tslint:disable-next-line:max-line-length
+    const sql = { 'sql': 'SELECT * from stock' };
+    this.sql.postQry(sql, 'phpQuery/queryArgRJson.php').then((result) => {
+      this.stock = result;
+      this.remainingCalculate();
+    });
   }
 
   getTypeFromDB() {
-    const sql = { 'sql': 'SELECT * FROM printtype' };
-    // console.log(sql);
-    this.sql.postRequest('phpQuery/queryArgRJson.php', sql).subscribe(
-      response => {
-        console.log(response.json());
-        this.inResult = response.json();
-        for (let i = 0; i < this.inResult.length; i++) {
-          console.log(this.inResult[i].type);
-          this.typeArray.push({ 'type': this.inResult[i].type });
-        }
-        console.log(this.typeArray);
-        this.getTypeSFTtotalFromDB();
-      },
-      err => {
-        console.log(err);
-        this.message.add({ severity: 'error', summary: 'Problem Found', detail: err });
-      }
-    );
+    // tslint:disable-next-line:max-line-length
+    const sql = { 'sql': 'SELECT printdetails.PrintType as type, sum(printdetails.sft) as sftsum from printdetails GROUP BY printdetails.PrintType' };
+    this.sql.postQry(sql, 'phpQuery/queryArgRJson.php').then((result) => {
+      this.typeAndSumResult = result;
+    });
   }
 
-  getTypeSFTtotalFromDB() {
-    this.typeArrayTemp = this.typeArray;
-    for (let i = 0; i < this.typeArrayTemp.length; i++) {
-      // tslint:disable-next-line:max-line-length
-      const sql = { 'sql': 'SELECT sum(sft) as sft FROM printdetails WHERE printtype ="' + this.typeArrayTemp[i].type + '"' };
-      this.sql.postRequest('phpQuery/queryArgRJson.php', sql).subscribe(
-        response => {
-          console.log(response.json());
-          this.inResult2 = response.json();
-          this.typeSFTtotalArray.push({ 'typeSum': this.inResult2[0].sft });
-
-        },
-        err => {
-          console.log(err);
-          this.message.add({ severity: 'error', summary: 'Problem Found', detail: err });
-        }
-      );
+  // time return
+  geTime() {
+    const date = new Date();
+    let time = date.getHours() + ''; // convert number to string and it will again use as string
+    if (Number(time) >= 12) {
+      const pm = Number(date.getHours()) - 12;
+      time = pm + ':' + date.getMinutes() + ':' + date.getSeconds() + 'pm';
+    } else {
+      time = time + ':' + date.getMinutes() + ':' + date.getSeconds() + 'am';
     }
-    console.log(this.typeSFTtotalArray);
+    return time;
+  }
+
+  // date return
+  geDate() {
+    const date = new Date();
+    const presentDate = date.getFullYear() + '/' + date.getMonth() + '/' + date.getDay();
+    return presentDate;
+  }
+
+  getRandomID() {
+    const date = new Date();
+    const uuid = UUID.UUID();
+    // tslint:disable-next-line:max-line-length
+    const id = date.getHours().toString() + date.getMinutes().toString() + date.getMilliseconds().toString() + uuid.split('-')[2];
+    return id;
+  }
+
+  clearinput() {
+    this.details = undefined;
+    this.size = undefined;
+    this.media = undefined;
+    this.mediaWhere = undefined;
   }
 }
